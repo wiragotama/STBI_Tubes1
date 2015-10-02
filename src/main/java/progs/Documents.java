fd40316281b56810450ef0f4277149676c1031a7
mario.filino <mario.filino@gmail.com>
2015-10-02 15:28:14 +0700

    add stopwords removal

--- src/main/java/progs/Documents.java
+++ src/main/java/progs/Documents.java
@@ -1,59 +1,122 @@
 package progs;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
 import java.io.Reader;
 import java.io.StringReader;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Set;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
+import com.aliasi.tokenizer.EnglishStopTokenizerFactory;
 import com.aliasi.tokenizer.IndoEuropeanTokenizerFactory;
+import com.aliasi.tokenizer.LowerCaseTokenizerFactory;
 import com.aliasi.tokenizer.PorterStemmerTokenizerFactory;
+import com.aliasi.tokenizer.StopTokenizerFactory;
 import com.aliasi.tokenizer.Tokenization;
 import com.aliasi.tokenizer.TokenizerFactory;
+import com.google.common.collect.ImmutableSet;
 
 import edu.stanford.nlp.ling.HasWord;
 import edu.stanford.nlp.process.DocumentPreprocessor;
 import parser.Parser;
+import vsm.VSM;
 
 public class Documents {
 	private List<String[]> documents;
 	
-	public Documents(String filePath){
+	/**
+	 * Constructor
+	 * @param documentPath
+	 * @param stopWordsPath
+	 */
+	public Documents(String documentPath, String stopWordsPath){
 		documents = new ArrayList<>();
 		
+		List<String> stopWords = loadStopWords(stopWordsPath);
+		
 		Parser parser = new Parser();
-		parser.parseDocuments(filePath);
-		tokenizeDocuments(parser.getDocuments());
-		removeStopWords();
+		parser.parseDocuments(documentPath);
+		tokenizeDocuments(parser.getDocuments(), stopWords);
 		stem();
 	}
 	
-	private void tokenizeDocuments(List<String> documents){
+	/**
+	 * Tokenize all documents
+	 * @param documents
+	 * @param stopwords
+	 */
+	private void tokenizeDocuments(List<String> documents, List<String> stopwords){
 		for(String document : documents){
-			this.documents.add(tokenize(document));
+			this.documents.add(tokenize(document, stopwords));
 		}
 	}
 	
-	private String[] tokenize(String input){
-		String[] token = null;
+	/**
+	 * Tokenize String, including lowercasing token and stopwords removal
+	 * @param input
+	 * @param stopWords
+	 * @return Array of tokenized String
+	 */
+	private String[] tokenize(String input, List<String> stopWords){
+		TokenizerFactory TOKENIZER_FACTORY = IndoEuropeanTokenizerFactory.INSTANCE;
+        TokenizerFactory lowercasetokenizer = new LowerCaseTokenizerFactory(TOKENIZER_FACTORY);
+        
+        Set<String> stopwords = ImmutableSet.copyOf(stopWords);
+        StopTokenizerFactory fstop = new StopTokenizerFactory(lowercasetokenizer, stopwords);
+        TokenizerFactory tokenizer = new EnglishStopTokenizerFactory(fstop);
+        
+    	Tokenization tk = new Tokenization(input, tokenizer);
+    	String result[] = tk.tokens();
+    	
+    	return result;
+	}
+	
+	/**
+	 * Load stop-words list from external file
+	 * @param filePath
+	 * @return List of stopwords
+	 */
+	private List<String> loadStopWords(String filePath){
+		File stopWords = new File(filePath);
+		String line = null;
+		List<String> stopwords = new ArrayList<>();
+		BufferedReader reader = null;
+				
+		try {
+			reader = new BufferedReader(new FileReader(stopWords));
+		} catch (FileNotFoundException e) {
+			System.out.println(filePath + " is not found");
+		}
 		
-    	Reader reader = new StringReader(input);
-		DocumentPreprocessor dp = new DocumentPreprocessor(reader);
-		
-		for(List<HasWord> sentence : dp){
-			token = new String[sentence.size()];
-			for(int i = 0; i < sentence.size(); i++){
-				token[i] = sentence.get(i).word();
+		try {
+			while((line = reader.readLine()) != null){
+				String split[] = line.split(" ");
+				stopwords.add(split[0]);
 			}
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
 		}
 		
-		return token;
+		try {
+            reader.close();
+        } catch (IOException ex) {
+            Logger.getLogger(VSM.class.getName()).log(Level.SEVERE, null, ex);
+        }
+		 
+		return stopwords;
 	}
 	
-	private void removeStopWords(){
-		
-	}
-	
+	/**
+	 * Stem String using Porter Stemmer
+	 */
 	private void stem(){
 		TokenizerFactory f1 = IndoEuropeanTokenizerFactory.INSTANCE;
 		TokenizerFactory porter = new PorterStemmerTokenizerFactory(f1);
@@ -66,6 +129,9 @@ public class Documents {
 		}
 	}
 	
+	/**
+	 * Returns a string representation of the object
+	 */
 	public String toString(){
 		String output = "";
 		
@@ -77,16 +143,25 @@ public class Documents {
 		return output;
 	}
 	
+	/**
+	 * Get all tokenized documents
+	 * @return documents
+	 */
 	public List<String[]> getDocuments(){
 		return this.documents;
 	}
 	
+	/**
+	 * Get document at a specified index
+	 * @param index
+	 * @return document
+	 */
 	public String[] getDocument(int index){
 		return this.documents.get(index);
 	}
 	
 	public static void main(String[] args){
-		Documents docs = new Documents("test_collections/cisi/cisi.all");
+		Documents docs = new Documents("test_collections/cisi/cisi.all", "custom.stopword");
 		System.out.println(docs);
 	}
 }
