f078ce886863ff0c5dd0db612183f2f7b64c5f5c
mario.filino <mario.filino@gmail.com>
2015-10-09 10:34:18 +0700

    add terms generator from documents

--- src/main/java/progs/Documents.java
+++ src/main/java/progs/Documents.java
@@ -57,8 +57,32 @@ public class Documents {
 		return this.documents.get(index);
 	}
 	
+	/**
+	 * Get terms from all combined documents
+	 * @return List of terms
+	 */
+	public List<String> getTerms(){
+		List<String> terms = new ArrayList<>();
+		
+		for(String[] document : documents){
+			for(int i = 0; i < document.length; i++){
+				terms.add(document[i]);
+			}
+		}
+		
+		return terms;
+	}
+	
+	/**
+	 * Clear memory
+	 */
+	public void clear(){
+		this.documents.clear();
+	}
+	
 	public static void main(String[] args){
 		Documents docs = new Documents("test_collections/cisi/cisi.all", "custom.stopword", true);
 		System.out.println(docs);
+		System.out.println(docs.getTerms());
 	}
 }
